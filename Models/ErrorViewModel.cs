2e65f93d0cd4c29c629104ff60a8ace2c8b0139d
Jonathan Schultz <jonathan.a.schultz@gmail.com>
2017-08-25 09:50:58 -0400

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace video_rental.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
